c2affe932c0b6fc17fde3fd66ddf05f74cfc0f5b
Michael Hock <michael.hock@uni-bamberg>
2024-10-20 17:39:25 +0200

    Updated documentation, rename item selection function

--- R/simpredsel.R
+++ R/simpredsel.R
@@ -1,9 +1,9 @@
-#' Simple Predictor Selection
-#'
-#' The package provides functions for simple (naive) predictor selection and cross-validation.
-#'
-#' @name simpredsel
-#'
+### #' Simple Predictor Selection
+### #'
+### #' Functions for simple (naive) predictor selection and cross-validation
+#### #'
+### #' @name simpredsel
+### #'
 #' @importFrom stats cor var
 ### #' @importFrom splitTools partition
 ### #' @importFrom pROC auc
@@ -11,7 +11,7 @@ NULL
 
 #' Compute AUCs for one or more predictors
 #'
-#' Compute AUCs with the first component of `x` as response and each of the remainings components as predictors. The first component must be binary (coded as 0 for absence, and 1 for presence of a feature). The computation is done via [pROC::auc()].
+#' Compute AUCs (areas under the receiver operating characteristic curve) for the first component of `x` as response and each of its remainings components as predictors. The first component must be binary (coded as 0 for absence, and 1 for presence of a feature). The computation is done via [pROC::auc()].
 #'
 #' @param x a data frame
 #' @returns a vector of AUC values
@@ -27,9 +27,15 @@ compute_aucs <- function(x) {
     myauc
 }
 
-#' Find predictors in a set that contribute to the predictive validity of the sum score of a subset of the predictors
+#' Predictor selection
 #'
-#' The search for the best predictors is done by a simple forward selection. The procedure starts with selecting the predictor with the highest validity. Subsequently, predictors with the highest incremental validity are added. In each step, a sum score of all selected preditors is computed whose association with the criterion is assessed. Association may be measured by the area under the ROC curve or by the correlation. All predictors should be coded in the same direction as the criterion, so that the (true) correlation between the predictor and the criterion is positive. By default, predictors with nonpositive correlations with the criterion are eliminated from the predictor set.
+#' Find predictors in a set that contribute to the predictive validity of the sum score of the selected predictors.
+#'
+#' @details The search for predictors is done by a forward selection procedure. The procedure starts with selecting the predictor with the highest validity (association with the criterion) and then successively adds predictors to the selected set that maximize the *incremental* validity of the sum score.
+#'
+#' Association may be measured by the area under the ROC curve or by the correlation coefficient. All predictors should be coded in the same direction as the criterion, so that the correlation between the predictor and the criterion is positive.
+#'
+#' By default, predictors with nonpositive correlations with the criterion are eliminated from the predictor set. Predictors with no variance are also removed.
 #'
 #' @param predictors data frame of predictor variables
 #' @param criterion a numeric vector representing the criterion (dependent variable)
@@ -42,13 +48,17 @@ compute_aucs <- function(x) {
 #' - `k` (the number of predictors used for the final sum score),
 #' - `assoc` (the association of the final sum score with the criterion),
 #' - `sel_pred_names` (the names of the selected predictors),
-#' - `final_sum_score` (the final sum score of the predictors)
+#' - `final_sum_score` (the final sum score of the selected predictors)
+#'
+#' If no valid predictors are found NULL is returned.
 #'
 #' @export
-forward_selection_preds <- function(predictors, criterion, assoc_measure = c("auc", "cor"), only_positive = TRUE, show_progress = TRUE) {
-    # requires function `compute_aucs` (which requires pROC)
+pred_sel <- function(predictors, criterion, assoc_measure = c("auc", "cor"), only_positive = TRUE, show_progress = TRUE) {
     assoc_measure <- match.arg(assoc_measure)
     stopifnot(is.data.frame(predictors), is.atomic(criterion))
+    if (assoc_measure == "auc") {
+        stopifnot(all(criterion %in% c(0, 1)))
+    }
     # Remove predictors that have no variance.
     # Should that be done *before* the CV iterations?
     n_pred <- ncol(predictors)
@@ -68,6 +78,12 @@ forward_selection_preds <- function(predictors, criterion, assoc_measure = c("au
             message(k0, " predictors removed because of nonpositive correlation with the criterion")
         }
     }
+    # Do we still have predictors?
+    if (ncol(predictors) == 0) {
+        warning("no predictors with positive variance/correlation remaining")
+        return(NULL)
+    }
+
     # Main loop
     best_pred <- 0
     old_best_assoc <- 0
@@ -89,8 +105,9 @@ forward_selection_preds <- function(predictors, criterion, assoc_measure = c("au
         best_pred <- remaining_preds[, best_pred_idx]
         best_assoc <- assocs[best_pred_idx]
         best_pred_name <- names(assocs)[best_pred_idx]
-
-        if (best_assoc < old_best_assoc) {
+        # Return if there is no validity increment
+        if (best_assoc <= old_best_assoc) {
+            names(old_best_assoc) <- "assoc"
             output <- list(k = i - 1,
                 assoc = old_best_assoc,
                 sel_pred_names = sel_pred_names,
@@ -116,12 +133,14 @@ forward_selection_preds <- function(predictors, criterion, assoc_measure = c("au
 
 #' Monte Carlo Cross-Validation
 #'
-#' Stratified Monte Carlo (repeated random sub-sampling) cross-validation for predictor selections with [forward_selection_preds].
+#' Stratified Monte Carlo (repeated random sub-sampling) cross-validation for predictor selections with [pred_sel].
 #'
 #' @param x data frame with predictors
 #' @param criterion criterion (to be predicted variable)
 #' @param n number of Monte Carlo runs (i.e., training/validation samples drawn)
 #' @param assoc_measure type of association measure. May be `auc` (area under the ROC) or `cor` (correlation). For the former, the criterion should be binary (coded for 0, 1 for presence of a feature).
+#' @param only_positive consider only predictors with positive correlations with the criterion?
+#' @param show_progress show progress?
 #'
 #' @returns A list containing the components
 #'
@@ -134,7 +153,7 @@ forward_selection_preds <- function(predictors, criterion, assoc_measure = c("au
 #' @references Xu, Q.-S., & Liang, Y.-Z. (2001). Monte Carlo cross validation. *Chemometrics and Intelligent Laboratory Systems*, *56*(1), 1–11. https://doi.org/10.1016/S0169-7439(00)00122-2
 
 #' @export
-mc_crossvalidation <- function(x, criterion, n = 100L, assoc_measure = c("auc", "cor")) {
+mc_crossvalidation <- function(x, criterion, n = 100L, assoc_measure = c("auc", "cor"), only_positive = TRUE, show_progress = FALSE) {
     assoc_measure <- match.arg(assoc_measure)
     stopifnot(is.data.frame(x), is.atomic(criterion), is.numeric(n), n > 0)
     k <- numeric(n)
@@ -147,17 +166,27 @@ mc_crossvalidation <- function(x, criterion, n = 100L, assoc_measure = c("auc", 
         criterion_train <- criterion[mysplit$train]
         valid_x <- x[mysplit$valid,]
         criterion_valid <- criterion[mysplit$valid]
-        res <- forward_selection_preds(train_x, criterion_train,
-            assoc_measure = assoc_measure)
-        k[i] <- res$k
-        assoc[i] <- res$assoc
-        y <- rowSums(valid_x[res$sel_pred_names])
-        if (assoc_measure == "auc") {
-            stopifnot(all(criterion %in% c(0, 1)))
-            assoc_valid[i] <- pROC::auc(criterion_valid, y, direction = "<",
-                quiet = TRUE)
+        res <- pred_sel(train_x, criterion_train,
+            assoc_measure = assoc_measure,
+            only_positive = only_positive,
+            show_progress = show_progress)
+        if (is.null(res$k)) {
+            warning("No results for predictor selection in MC iteration step ", i)
+            k[i] <- 0
+            assoc[i] <- NA
+            y <- NA
+            assoc_valid[i] <- NA
         } else {
-            assoc_valid[i] <- cor(criterion_valid, y)
+            k[i] <- res$k
+            assoc[i] <- res$assoc
+            y <- rowSums(valid_x[res$sel_pred_names])
+            if (assoc_measure == "auc") {
+                stopifnot(all(criterion %in% c(0, 1)))
+                assoc_valid[i] <- pROC::auc(criterion_valid, y, direction = "<",
+                    quiet = TRUE)
+            } else {
+                assoc_valid[i] <- cor(criterion_valid, y)
+            }
         }
     }
     return(list(assoc = assoc, assoc_valid = assoc_valid, k = k))
